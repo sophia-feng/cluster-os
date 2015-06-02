d8edd2f5ad67338a10749fae2501f8866d1796bb
Sophia Feng <sfeng@HW11011.local>
2015-06-01 18:50:25 -0700

    change keypair to qe

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export SSH_ARGS="-o StrictHostKeyChecking=no -i /tmp/hw-dev-keypair.pem"
+export SSH_ARGS="-o StrictHostKeyChecking=no -i /tmp/hw-qe-keypair.pem"
 export HBASE_CONF_DIR=/etc/hbase/conf/
 export HDP=/usr/hdp/current/
